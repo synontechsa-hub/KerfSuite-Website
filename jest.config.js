b7aa2cddc046e72fa96760b7a6e3f4eea551634b
synontechsa-hub <synontech.sa@gmail.com>
2026-07-06 16:21:21 +0200

    chore: update jest config with proper type annotation, module path prefix, and root directory configuration

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
   testMatch: ['**/tests/**/*.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.json',
+      rootDir: '.',
     }],
   },
 };
